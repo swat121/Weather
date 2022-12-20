76324543618bac19475274ab9eb31499523bae66
swat121 <stark2542@gmail.com>
2022-12-20 19:14:41 +0200

    Add: new city search functionality

--- src/main/java/com/example/weather/controller/WeatherController.java
+++ src/main/java/com/example/weather/controller/WeatherController.java
@@ -1,20 +1,35 @@
 package com.example.weather.controller;
 
+import com.example.weather.model.DataResponse;
 import com.example.weather.service.WeatherService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
 public class WeatherController {
     private WeatherService service;
 
-    @GetMapping("/")
-    public String getTemperature(Model model){
+    @GetMapping("/city")
+    public String getTemperature(Model model) {
         model.addAttribute("countryName", service.getCountyName());
         model.addAttribute("data", service.getDataResponse().getMain());
         return "main";
+    }
+
+    @GetMapping("/")
+    public String find(Model model) {
+        model.addAttribute("dataResponse", new DataResponse());
+        return "find";
+    }
+
+    @PostMapping("/")
+    public String findProduct(@ModelAttribute("dataResponse") DataResponse dataResponse) {
+        service.setCountryName(dataResponse.getName());
+        return "redirect:/city";
     }
 }
--- src/main/java/com/example/weather/service/WeatherService.java
+++ src/main/java/com/example/weather/service/WeatherService.java
@@ -2,7 +2,6 @@ package com.example.weather.service;
 
 import com.example.weather.config.UrlConfig;
 import com.example.weather.model.DataResponse;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,12 +15,16 @@ public class WeatherService {
     private final String url = "https://api.openweathermap.org/data/2.5/";
     private final UrlConfig urlConfig;
 
+    public void setCountryName(String countryName) {
+        urlConfig.setCountryName(countryName);
+    }
+
     public DataResponse getDataResponse() {
         URI uri = URI.create(url).resolve(String.format("weather?q=%s&units=%s&appid=%s", urlConfig.getCountryName(), urlConfig.getUnits(), urlConfig.getId()));
         return getFromUrl(uri.toString(), DataResponse.class);
     }
 
-    public String getCountyName(){
+    public String getCountyName() {
         return urlConfig.getCountryName();
     }
 
