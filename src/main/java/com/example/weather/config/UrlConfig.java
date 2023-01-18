8abfc3ceb6fa9b4cbf21426f3a4302d98e6cbb25
swat121 <stark2542@gmail.com>
2023-01-18 11:24:55 +0200

    Code cleaning

--- src/main/java/com/example/weather/config/UrlConfig.java
+++ src/main/java/com/example/weather/config/UrlConfig.java
@@ -3,8 +3,6 @@ package com.example.weather.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.Map;
-
 @Data
 @ConfigurationProperties(prefix = "service")
 public class UrlConfig {
